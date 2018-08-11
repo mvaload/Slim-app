573ca41a7f8ad7f532a51ad27b4e2517f72cd435
Moiseenko Vladimir <mvaload@gmail.com>
2018-08-11 15:01:23 +0300

    add new handler

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 
+namespace App;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+$users = Generator::generate(100);
 
 $configuration = [
     'settings' => [
@@ -14,25 +18,29 @@ $container = $app->getContainer();
 $container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 
 $app->get('/', function ($request, $response) {
-    $response->write('Welcome to Slim!');
-    return $response;
+    return $this->renderer->render($response, 'index.phtml');
 });
 
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id']];
-    return $this->renderer->render($response, 'users/show.phtml', $parems);
+$app->get('/users', function ($request, $response) use ($users) {
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 5);
+    $offset = ($page - 1) * $per;
+
+    $sliceOfUsers = array_slice($users, $offset, $per);
+    $params = [
+        'users' => $sliceOfUsers,
+        'page' => $page
+    ];
+    return $this->renderer->render($response, 'users/index.phtml', $params);
 });
 
-$app->post('/users', function ($request, $response) {
-    $response->write('POST /users');
-    return $response;
-});
-
-$app->get('/courses/{id}', function ($request, $response, array $args) {
-    $id = $args['id'];
-    $response->write("Course id: {$id}");
-
-    return $response;
+$app->get('/users/{id}', function ($request, $response, $args) use ($users) {
+    $id = (int) $args['id'];
+    $user = collect($users)->first(function ($user) use ($id) {
+        return $user['id'] == $id;
+    });
+    $params = ['user' => $user];
+    return $this->renderer->render($response, 'users/show.phtml', $params);
 });
 
 $app->run();
